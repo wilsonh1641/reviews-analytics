b9a28285e88e87ac355dca3ccd03c959912752a3
Wilson Hung <Wilsonh1641@gmail.com>
2022-02-09 16:17:50 +0800

    add filter

--- read.py
+++ read.py
@@ -14,3 +14,12 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('平均是', sum_len/len(data))
+
+# 篩選概念
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
